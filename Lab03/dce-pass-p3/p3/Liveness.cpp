42a3c06ffa486a41b03a4d4d2b4dac80acc44d0a
Andre Vitor Terron <andrev.terron@gmail.com>
2015-05-20 18:17:14 -0300

    Writing the algorithm for BB In and Out calculation (incomplete)

--- Lab03/dce-pass-p3/p3/Liveness.cpp
+++ Lab03/dce-pass-p3/p3/Liveness.cpp
@@ -1,6 +1,6 @@
 #include "llvm/IR/Instructions.h"
 #include "Liveness.h"
-
+#include <utility>
 #include <unistd.h>
 #include <stdio.h>
 
@@ -24,17 +24,20 @@ errs() << "IN1" << '\n';
       for (BasicBlock::iterator i = b->begin(), e = b->end(); i != e; ++i) {
 
          errs() << "INS:" << *i << '\n';
+         
          for (Instruction::op_iterator o = i->op_begin(), oe = i->op_end(); o != oe; ++o) {
             Value *v = *o;
             /*if(isa<Instruction>(*v) || isa<Argument>(*v)){
-         		errs() << "\tOPE:" << *v << '\n';
-         	}*/
-         	if(isa<Instruction>(*v)){
-         		errs() << "\tOPE-I:" << *v << '\n';
-         	}
-         	else if(isa<Argument>(*v)){
-         		errs() << "\tOPE-A:" << *v << '\n';
-         	}
+              errs() << "\tOPE:" << *v << '\n';
+            }*/
+            if(isa<Instruction>(*v)){
+                errs() << "\tOPE-I:" << *v << '\n';
+                s.use.insert(v);
+            }
+            
+            else if(isa<Argument>(*v)){
+              errs() << "\tOPE-A:" << *v << '\n';
+            }
          }
       }
           
@@ -60,10 +63,43 @@ errs() << "IN1" << '\n';
 
 void Liveness::computeBBInOut(Function &F){
 errs() << "IN2" << '\n';
+  for (Function::iterator b = F.begin(), e = F.end(); b != e; ++b) {
+
+    LivenessInfo s;
+    for (BasicBlock::iterator i = b->begin(), e = b->end(); i != e; ++i) {
+    }
+  }
 }
 
 void Liveness::computeIInOut(Function &F) {
-errs() << "IN3" << '\n';
+  errs() << "IN3" << '\n';
+  
+  LivenessInfo *s;
+  set in2, out2;
+  int changed;
+  
+  for (Function::iterator b = F.begin(), e = F.end(); b != e; ++b) {
+    s = &*bbLivenessMap.find(&*b);
+    s->in.clear();
+    s->out.clear();
+  }
+  do {
+    changed = 0;
+    // This for is going backwards
+    for (Function::iterator b = F.end(), e = F.begin(); b != e; --b) {
+      s = &*bbLivenessMap.find(&*b);
+      in2 = s->in;
+      out2 = s->out;
+      
+      // TODO : do something
+      // in[n] = use[n] U (out[n] - def[n])
+      // out[n] = U(s in succ[n]) in[s]
+      
+      if (in2 != s->in || out2 != s->out) {
+        changed++;
+      }
+    }
+  } while (changed);
 }
 
 bool Liveness::runOnFunction(Function &F) {
