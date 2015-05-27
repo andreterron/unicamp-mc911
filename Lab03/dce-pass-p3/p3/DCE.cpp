20da4abaf7e16c0848a9cfc186bb398068d842a9
Lucas Lima <lucaslima052@gmail.com>
2015-05-27 18:10:22 -0300

    DCE-Liveness add

---
+++ Lab03/dce-pass-p3/p3/DCE.cpp
@@ -0,0 +1,27 @@
+#include "Liveness.h"
+using namespace llvm;
+namespace {
+   struct DCE : public FunctionPass {
+      static char ID;
+    	DCE() : FunctionPass(ID) {}
+
+    	virtual bool runOnFunction(Function &F) {
+        // Liveness &L = getAnalysis<Liveness>();
+
+        //...
+
+
+         return false;
+      }
+
+    	virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+         //AU.addRequired<Liveness>();
+		   return;
+    	}
+   };
+}
+
+char DCE::ID = 0;
+
+RegisterPass<DCE> X("DCE", "DCE", false, false);
+
