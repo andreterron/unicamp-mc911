49cd2fde508a5e29ec5724b42cc3dacec81926f2
Andre Vitor Terron <andrev.terron@gmail.com>
2015-05-27 16:27:58 -0300

    Instruction In and Out, and isLiveOut

--- Lab03/dce-pass-p3/p3/Liveness.cpp
+++ Lab03/dce-pass-p3/p3/Liveness.cpp
@@ -11,7 +11,8 @@ void print_elem(const Value* i) {
 }
 
 bool Liveness::isLiveOut(Instruction *I, Value *V){
-    return false;
+    LivenessInfo *info = &((&*iLivenessMap.find(I))->second);
+    return (info->out.find(V) != info->out.end());
 }
 
 void Liveness::computeBBDefUse(Function &F){
@@ -23,31 +24,37 @@ errs() << "IN1" << '\n';
       // TODO : Criar um liveness para cada bloco
       // LivenessInfo info;
       // bbLivenessMap.insert(make_pair(b, info));
-      // s = &((&*bbLivenessMap.find(&*b))->second);
-      LivenessInfo s;
+      // bbInfo = &((&*bbLivenessMap.find(&*b))->second);
+      LivenessInfo bbInfo;
       
       for (BasicBlock::iterator i = b->begin(), e = b->end(); i != e; ++i) {
 
+         LivenessInfo iInfo;
+        
          errs() << "INS:" << *i << '\n';
          
-         s.def.insert(i);
+         bbInfo.def.insert(i);
+         iInfo.def.insert(i);
          
          for (Instruction::op_iterator o = i->op_begin(), oe = i->op_end(); o != oe; ++o) {
             Value *v = *o;
             
             if(isa<Instruction>(*v)){
                 errs() << "\tOPE-I:" << *v << '\n';
-                s.use.insert(v);
+                bbInfo.use.insert(v);
+                iInfo.use.insert(v);
             }
             
             else if(isa<Argument>(*v)){
               errs() << "\tOPE-A:" << *v << '\n';
-               s.use.insert(v);
+               bbInfo.use.insert(v);
+               iInfo.use.insert(v);
             }
          }
+         iLivenessMap.insert(std::make_pair(&*i, iInfo));
       }
 
-      bbLivenessMap.insert(std::make_pair(&*b, s));
+      bbLivenessMap.insert(std::make_pair(&*b, bbInfo));
 
    }
 }
@@ -109,6 +116,61 @@ void Liveness::computeIInOut(Function &F) {
     for (BasicBlock::iterator i = b->begin(), e = b->end(); i != e; ++i) {
     }
   }
+  
+  LivenessInfo *info, *sInfo;
+  std::set<const Value *> in2, out2, diff;
+  //std::set<const Value *>::iterator it;
+  int changed;
+  
+  
+  for (Function::iterator b = F.begin(), e = F.end(); b != e; ++b) {
+    for (BasicBlock::iterator i = b->begin(), ie = b->end(); i != ie; ++i) {
+      info = &((&*iLivenessMap.find(&*i))->second);
+      info->in.clear();
+      info->out.clear();
+    }
+  }
+  do {
+    changed = 0;
+    // This for is going backwards
+    for (Function::iterator b = F.end(), e = F.begin(); b != e; --b) {
+      for (BasicBlock::iterator i = b->end(), ie = b->begin(); i != ie; --i) {
+        info = &((&*iLivenessMap.find(&*i))->second);
+        in2 = info->in;
+        out2 = info->out;
+        diff.clear();
+        
+        // TODO : do something
+        
+        // out[n] = U(s in succ[n]) in[s]
+        info->out.clear();
+        for (Function::iterator s = F.end(); ; --s) {
+          for (BasicBlock::iterator i2 = b->end(); i != i2; --i2) {
+            sInfo = &((&*iLivenessMap.find(&*i2))->second);
+            info->out.insert(sInfo->in.begin(), sInfo->in.end());
+          }
+          if (s == b) {
+            break;
+          }
+        }
+        
+        // in[n] = use[n] U (out[n] - def[n])
+        info->in.clear();
+        info->in.insert(info->use.begin(), info->use.end());
+        set_difference(info->out.begin(),
+                                info->out.end(),
+                                info->def.begin(),
+                                info->def.end(),
+                                std::inserter(diff, diff.end()));
+        //diff.resize(it - diff.begin());
+        info->in.insert(diff.begin(), diff.end());
+        
+        if (in2 != info->in || out2 != info->out) {
+          changed++;
+        }
+      }
+    }
+  } while (changed);
 }
 
 bool Liveness::runOnFunction(Function &F) {
